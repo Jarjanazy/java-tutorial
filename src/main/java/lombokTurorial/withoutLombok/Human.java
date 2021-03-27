b46bbf9a1f86337db3c41370c340fb6eb4e7bbdf
jarjanazy <celil.cercenazi@gmail.com>
2021-03-27 19:19:51 +0300

    Add an entity for the non-lombok part

---
+++ src/main/java/lombokTurorial/withoutLombok/Human.java
@@ -0,0 +1,38 @@
+package lombokTurorial.withoutLombok;
+
+public class Human {
+    private final int id;
+    private String name;
+    private int ageInYears;
+
+    public Human(int id) {
+        this.id = id;
+    }
+
+    public Human(int id, String name, int ageInYears) {
+        this.id = id;
+        this.name = name;
+        this.ageInYears = ageInYears;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAgeInYears() {
+        return ageInYears;
+    }
+
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAgeInYears(int ageInYears) {
+        this.ageInYears = ageInYears;
+    }
+}
