0c446325110f14a067c94790c60f2d961f301fa4
jarjanazy <celil.cercenazi@gmail.com>
2021-04-26 22:25:30 +0300

    Add the clean code part to the refactoring tutorial

--- src/main/java/refactoringTutorial/CleanCode.java
+++ src/main/java/refactoringTutorial/CleanCode.java
@@ -10,35 +10,36 @@ public class CleanCode {
         this.items = items;
     }
 
+
     public void updateQuality() {
-        for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                        items[i].quality = items[i].quality - 1;
-                    }
-                }
-            } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
+        for (Item item : items) {
+            if (conditionsAreMetForQualityReduction(item))
+                item.quality = item.quality - 1;
+            else if (item.quality < 50)
+                performQualityAdditionProcess(item);
+        }
+    }
 
-                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
+    private void performQualityAdditionProcess(Item item) {
+        item.quality = item.quality + 1;
 
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-                    }
-                }
+        if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+
+            if (item.sellIn < 11 && item.quality < 50) {
+                item.quality = item.quality + 1;
+            }
+            if (item.sellIn < 6 && item.quality < 50) {
+                item.quality = item.quality + 1;
             }
         }
+    }
+
+    private boolean conditionsAreMetForQualityReduction(Item item) {
+        String name = item.name;
+        return !name.equals("Aged Brie") &&
+                !name.equals("Backstage passes to a TAFKAL80ETC concert") &&
+                item.quality > 0 &&
+                !name.equals("Sulfuras, Hand of Ragnaros");
     }
 
 }
