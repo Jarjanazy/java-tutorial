b4cd81abec4735106ee9823a345c76ec2ae4e5ed
jarjanazy <celil.cercenazi@gmail.com>
2021-06-26 14:15:01 +0300

    Add the code for the old way of parallelism

--- src/test/java/ParallelismTutorial.java
+++ src/test/java/ParallelismTutorial.java
@@ -1,26 +1,50 @@
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 class Job implements Runnable{
+    private final List<String> data;
+    public Job(List<String> data) {
+        this.data = data;
+    }
+
     @Override
     public void run() {
-        System.out.println("do work!!!!");
+        for (String s : data)
+            System.out.printf("Do work on %s%n", s);
     }
 }
 
 public class ParallelismTutorial {
     @Test
     public void oldWay(){
-        Job job = new Job();
-        List<Thread> threads = Arrays.asList(new Thread(job), new Thread(job), new Thread(job), new Thread(job));
-        for (Thread thread : threads){
-            thread.start();
-        }
+        List<String> data = getData();
+        for(List<String> batch : getDataChunks(data, 1000))
+            new Job(batch).run();
     }
 
+    private List<String> getData(){
+        return IntStream.
+                range(0, 10_000).
+                mapToObj(Integer::toString).
+                collect(Collectors.toList());
+    }
 
+    private List<List<String>> getDataChunks(List<String> data, int chunkSize){
+        List<List<String>> result= new ArrayList<>();
+        final AtomicInteger counter = new AtomicInteger();
 
+        for (String s : data) {
+            if (counter.getAndIncrement() % chunkSize == 0)
+                result.add(new ArrayList<>());
+            result.get(result.size() - 1).add(s);
+        }
+        return result;
+    }
 
 }
